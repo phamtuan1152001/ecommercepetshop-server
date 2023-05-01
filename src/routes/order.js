d5ecc3fd0a8ef6abb985e69d127f525f6ea1d488
phamtuan11520011 <phamtuan1152001@gmail.com>
2023-05-02 01:25:05 +0700

    fix

--- src/routes/order.js
+++ src/routes/order.js
@@ -6,35 +6,35 @@ const OrderController = require("../app/controllers/OrderController");
 // Create order
 router.post(
   "/create-order",
-  // [authJwt.verifyToken, authJwt.isAdmin],
+  [authJwt.verifyToken, authJwt.isAdmin],
   OrderController.createOrder
 );
 
 // Get list order
 router.post(
   "/get-list-order",
-  // [authJwt.verifyToken, authJwt.isAdmin],
+  [authJwt.verifyToken, authJwt.isAdmin],
   OrderController.getListOrder
 );
 
 // Get detail order
 router.get(
   "/get-detail-order/:id",
-  // [authJwt.verifyToken, authJwt.isAdmin],
+  [authJwt.verifyToken, authJwt.isAdmin],
   OrderController.getDetailOrder
 );
 
 // Delete detail order
 router.delete(
   "/delete-detail-order/:id",
-  // [authJwt.verifyToken, authJwt.isAdmin],
+  [authJwt.verifyToken, authJwt.isAdmin],
   OrderController.deleteDetailOrder
 );
 
 // Update detail order
 router.put(
   "/update-detail-order/:id",
-  // [authJwt.verifyToken, authJwt.isAdmin],
+  [authJwt.verifyToken, authJwt.isAdmin],
   OrderController.updateOrder
 );
 
